bbfc05367971c97276c08604e1e98e799c8cbf6e
Krishnasatish903 <krishnasatish.yaragogu@gmail.com>
2026-02-10 20:38:45 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+
+import { clickAction, radioBtnSelected, suggestionListDropDown, verifyText } from "../../../utilities/genericUtilities";
+import{radio1Btn, countriesList} from "../../../pageObjects/homePage/homePage.json"
+import {radio1} from "../../../testData/homepageData/homePageData.json"
+
+describe('Cyprss Parctice for using Rahul Shetty Page ', ()=>{
+
+it('Test_Case_01 to check the Radio buttons',()=>{
+
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+clickAction({locator: radio1Btn});
+radioBtnSelected({locator: radio1Btn});
+suggestionListDropDown({locator: countriesList, value: "Ind", exactText: "India"})
+
+
+})
+
+})
